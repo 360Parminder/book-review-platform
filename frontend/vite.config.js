27fd443ebfdd10bc0aececc2fe80c365c72e1a3b
360Parminder <yesask8@gmail.com>
2025-07-30 03:34:02 +0530

    feat: Replace Vite logo with custom logo and update favicon reference in index.html

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_URL,
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
-})
+}))
